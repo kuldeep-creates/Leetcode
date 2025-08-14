02cb6f2cfd473ebf194f70e9a309d0e68208ee97
Kuldeep Jangid <kuldeepjangid2008@gmail.com>
2025-08-15 04:38:58 +0530

    Time: 0 ms (100%), Space: 24.4 MB (84.98%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int c=0;
+        for (auto it:hours){
+            if(it>=target){
+                c++;
+            }
+        }
+        return c;
+        
+    }
+};
