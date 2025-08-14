60316f2137d74ddc5eaf268145d8f1e1a21f5c8c
Kuldeep Jangid <kuldeepjangid2008@gmail.com>
2025-08-15 03:46:22 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.55%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int z = 0;
+        while (n > 0) {
+            n /= 5;
+            z += n;
+        }
+        return z;
+    }
+};
