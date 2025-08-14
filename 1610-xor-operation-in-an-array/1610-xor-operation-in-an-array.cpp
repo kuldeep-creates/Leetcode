0e6975b3e181cbdd1825a16637b3572ebe6ce0fd
Kuldeep Jangid <kuldeepjangid2008@gmail.com>
2025-08-15 04:10:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.42%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int>nums;
+        for(int i=0;i<n;i++){
+            nums.push_back(start+2*i);
+        }
+        int ans=nums[0];
+        for(int k=1;k<nums.size();k++){
+            ans^=nums[k];
+        }
+        return ans;
+
+        
+    }
+};
